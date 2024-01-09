72588eda116860c4a8de90bfbd09a7f0a1216a60
Parker <pkroberg@gmail.com>
2024-01-08 23:55:49 -0700

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Portfolio</h1>
+      <nav>
+        <ul>
+          <li>
+            <a href="#about">About Me</a>
+          </li>
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
